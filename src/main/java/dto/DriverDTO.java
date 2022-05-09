defad8b9614a78beb837d3369929254a93b586a2
Stefan Todorov <stefan.milenov.todorov@gmail.com>
2022-05-09 19:17:19 +0300

    Added Lombok to DriverDTO.java

--- src/main/java/dto/DriverDTO.java
+++ src/main/java/dto/DriverDTO.java
@@ -1,27 +1,22 @@
 package dto;
 
 import constant.DriverQualification;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DriverDTO {
-
     private int id;
 
     private String firstName;
     private String lastName;
     private DriverQualification driverQualification;
     private double salary;
-
-    public DriverDTO() {
-
-    }
-
-    public DriverDTO(int id, String firstName, String lastName, DriverQualification driverQualification, double salary) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.driverQualification = driverQualification;
-        this.salary = salary;
-    }
 
     @Override
     public String toString() {
