869b9ad3049b0a9b5ec403f7aa1617715f0e0b84
Stefan Todorov <stefan.milenov.todorov@gmail.com>
2022-05-09 18:46:48 +0300

    Made changes in ClientDTO.java

--- src/main/java/dto/ClientDTO.java
+++ src/main/java/dto/ClientDTO.java
@@ -1,23 +1,22 @@
 package dto;
 
-public class ClientDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.swing.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO {
     private int id;
 
     private String firstName;
     private String lastName;
     private boolean hasPaid;
-
-    public ClientDTO() {
-
-    }
-
-    public ClientDTO(int id, String firstName, String lastName, boolean hasPaid) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.hasPaid = hasPaid;
-    }
 
     @Override
     public String toString() {
